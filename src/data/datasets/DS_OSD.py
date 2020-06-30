6ed5203ddc7a229bea0be1df0d28df0ed0fd691a
Anders <anmo@agro.au.dk>
2020-06-30 13:22:49 +0200

    Added class for full OSD dataset

--- src/data/datasets/DS_OSD.py
+++ src/data/datasets/DS_OSD.py
@@ -1,9 +1,12 @@
-import src.data.datasets.DS_Seeds as superDataset
-import numpy as np
-import PIL
-import tensorflow as tf
+import src.data.datasets.DS_OSD_base as superDataset
 import os
-import glob
+import random
+from operator import itemgetter
+# import numpy as np
+# import PIL
+# import tensorflow as tf
+# import os
+# import glob
 
 class Dataset(superDataset.Dataset):
     
@@ -11,39 +14,46 @@ class Dataset(superDataset.Dataset):
                     name = 'OSD',
                     rawFolder = 'data/raw/OSD',
                     processFolder = 'data/processed/OSD',
-                    numShards = 10):
+                    numShards = 162):
         super(Dataset, self).__init__(name = name, rawFolder = rawFolder, processFolder = processFolder, numShards = numShards)
 
-    def _get_filenames_and_classes(self, raw_folder, process_folder):
+    def _split_data_examples_to_shards(self, list_of_filenames, list_of_corresponding_class_names, list_of_unique_classes, num_shards, list_of_grouping_data):
 
-        data_root = os.path.join(raw_folder)
+        # list_of_lots = [os.path.split(os.path.split(filename)[0])[1] for filename in list_of_filenames]
+        list_of_labs = [os.path.normpath(filename).split('\\')[3] for filename in list_of_filenames]
+        labs = list(set(list_of_labs))
+        labs.sort()
 
-        list_of_filenames = []
-        list_of_corresponding_class_names = []
-        list_of_grouping_data = []
+        list_of_labs_species = [os.path.normpath(filename).split('\\')[3:5] for filename in list_of_filenames]
 
-        list_of_lab_directories = [f.path for f in os.scandir(data_root) if f.is_dir()]
-        for lab_directory in list_of_lab_directories:
+        unique_labs_species = [list(x) for x in set(tuple(x) for x in list_of_labs_species)]
 
-            lab_name = os.path.basename(lab_directory)
+        unique_labs_species = sorted(unique_labs_species, key=itemgetter(0))
 
-            list_of_species_directories = [f.path for f in os.scandir(lab_directory) if f.is_dir()]
-            for species_directory in list_of_species_directories:
+        shards_of_filenames = []
+        shards_of_corresponding_class_names = []
 
-                species_name = os.path.basename(species_directory)
-                
-                list_of_lot_directories = glob.glob(os.path.join(species_directory, 'Lot*'))
-                for lot_directory in list_of_lot_directories:
-                    list_of_filenames_in_lot = glob.glob(os.path.join(lot_directory,'*.tif'))
-                    list_of_filenames += list_of_filenames_in_lot
-                    list_of_corresponding_class_names += [[species_name] for i in range(len(list_of_filenames_in_lot))]
-                    list_of_grouping_data += [lab_name for i in range(len(list_of_filenames_in_lot))]
+        num_shards = []
 
-                # TODO: Loop through weed folders. Set species_name to weed
-                #  list_of_lot_directories = glob.glob(os.path.join(species_directory, 'Lot*'))
+        for lab in labs:
+            lab_species = [s for l,s in unique_labs_species if l == lab]
+            _num_shards = len(lab_species)*3*3
+            num_shards.append(_num_shards)
 
-        list_of_corresponding_class_names_T = list(map(list, zip(*list_of_corresponding_class_names))) # Transpose list of lists
-        list_of_unique_class_names = [list(set(class_names)) for class_names in list_of_corresponding_class_names_T]
-        list_of_unique_class_names[0].sort()
+            _list_of_filenames = [filename for filename, _lab in zip(list_of_filenames, list_of_labs) if _lab == lab]
+            _list_of_corresponding_class_names = [class_names for class_names, _lab in zip(list_of_corresponding_class_names, list_of_labs) if _lab == lab]
+            _list_of_grouping_data = [grouping_data for grouping_data, _lab in zip(list_of_grouping_data, list_of_labs) if _lab == lab]
 
-        return list_of_filenames, list_of_corresponding_class_names, list_of_unique_class_names, list_of_grouping_data
+            _shards_of_filenames, _shards_of_corresponding_class_names = super(Dataset, self)._split_data_examples_to_shards(_list_of_filenames, _list_of_corresponding_class_names, list_of_unique_classes, _num_shards, _list_of_grouping_data)
+
+            shards_of_filenames += _shards_of_filenames
+            shards_of_corresponding_class_names += _shards_of_corresponding_class_names
+
+            fob = open(os.path.join(self.processFolder, 'lab_shard_idx.txt'),'w')
+            shard_start = 1
+            for lab, n_shards in zip(labs, num_shards):
+                fob.write(lab + ', ' + ' '.join([str(i) for i in range(shard_start, shard_start+n_shards)]) + '\n')
+                shard_start = shard_start+n_shards
+            fob.close()
+
+        return shards_of_filenames, shards_of_corresponding_class_names
