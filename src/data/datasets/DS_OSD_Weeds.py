485b4d76ab4001d8c8289f9dc856a29b75531636
Anders <anmo@agro.au.dk>
2020-06-30 13:18:55 +0200

    Changed base class for OSD datasets

--- src/data/datasets/DS_OSD_Weeds.py
+++ src/data/datasets/DS_OSD_Weeds.py
@@ -1,4 +1,4 @@
-import src.data.datasets.DS_OSD as superDataset
+import src.data.datasets.DS_OSD_base as superDataset
 import numpy as np
 import PIL
 import tensorflow as tf
@@ -10,6 +10,6 @@ class Dataset(superDataset.Dataset):
     def __init__(self,
                     name = 'OSD, Weeds',
                     rawFolder = 'data/raw/OSD_Weeds',
-                    processFolder = 'data/processed/OSD_Weeds',
+                    processFolder = 'data/processed/OSD_Weeds2',
                     numShards = 1):
         super(Dataset, self).__init__(name = name, rawFolder = rawFolder, processFolder = processFolder, numShards = numShards)
