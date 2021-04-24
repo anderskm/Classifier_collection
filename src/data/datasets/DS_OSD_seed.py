cc576b73b3b84b357a6d5aa549cc9ee40de0eb1d
Anders <anmo@agro.au.dk>
2021-04-24 20:55:25 +0200

    Fixed paths and name

--- src/data/datasets/DS_OSD_seed.py
+++ src/data/datasets/DS_OSD_seed.py
@@ -11,9 +11,9 @@ from operator import itemgetter
 class Dataset(superDataset.Dataset):
     
     def __init__(self,
-                    name = 'OSD, new white clover focus',
-                    rawFolder = 'data/raw/OSD_new_wc',
-                    processFolder = 'data/processed/OSD_new_wc',
+                    name = 'OSD, seed only',
+                    rawFolder = 'data/raw/OSD_seed',
+                    processFolder = 'data/processed/OSD_seed',
                     numShards = 162):
         super(Dataset, self).__init__(name = name, rawFolder = rawFolder, processFolder = processFolder, numShards = numShards)
 
