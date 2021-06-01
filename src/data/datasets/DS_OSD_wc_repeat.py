976117e88b3ba9cd6da2b4381a98b18a8d701777
Anders <anmo@agro.au.dk>
2021-06-01 11:09:54 +0200

    Added white clover repetition dataset

---
+++ src/data/datasets/DS_OSD_wc_repeat.py
@@ -0,0 +1,18 @@
+import src.data.datasets.DS_OSD as superDataset
+import os
+import random
+from operator import itemgetter
+# import numpy as np
+# import PIL
+# import tensorflow as tf
+# import os
+# import glob
+
+class Dataset(superDataset.Dataset):
+    
+    def __init__(self,
+                    name = 'OSD, white clover repetition',
+                    rawFolder = 'data/raw/extracted_repeat__masked/Randers/Hvidkloever',
+                    processFolder = 'data/processed/OSD_wc_repeat',
+                    numShards = 10):
+        super(Dataset, self).__init__(name = name, rawFolder = rawFolder, processFolder = processFolder, numShards = numShards)
